34d3384e48cc6495a67f807814befa565828cd9d
Nekgate <Nekgate@gmail.com>
2024-04-29 17:01:43 +0100

    update

---
+++ api/v1/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Creating a flask app, register the blueprint app_viewss to flask instantiated app
+"""
+from os import getenv
+from flask import Flask
+from models import storage
+from api.v1.views import app_views
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appconetxt
+def teardown_engine(exception):
+    """
+    """
+    storage.close()
+
+
+    @app.errorhandler(404)
+    def not_found(error):
+        """
+        """
+        response = {"error": "Not found"}
+        return jsonify(response), 404
+
+if __name__ == "__main__":
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int (getenv('HBNB_API_PORT', 5000))
+    app.run(debug=True, hosts=HOST, port=PORT, threaded=True)
